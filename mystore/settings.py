d250f187d0120556005599dbf82db2714064ae7a
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-01-28 10:40:40 +0530

    modified:   mystore/__pycache__/settings.cpython-313.pyc modified:   mystore/settings.py modified:   requirements.txt

--- mystore/settings.py
+++ mystore/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mystore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mystore',
+        'USER': 'postgres',
+        'PASSWORD': 'smk4305@p',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
